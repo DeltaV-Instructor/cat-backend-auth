8b0181a09edb4ad89f31a46953e6d404fe4e6781
tektechnologies <info@tektechnologies.com>
2023-02-26 20:22:48 -0600

    updated code from documentation

--- auth.js
+++ auth.js
@@ -1,5 +1,35 @@
+/* eslint-disable no-var */
 const jwt = require('jsonwebtoken'); // auth
 const jwksClient = require('jwks-rsa'); // auth
+
+
+
+
+// =============== HELPER METHODS, pulled from the jsonwebtoken documentation =================== //
+//                 https://www.npmjs.com/package/jsonwebtoken                                     //
+
+// Define a client, this is a connection to YOUR auth0 account, using the URL given in your dashboard
+const client = jwksClient({
+  // this url comes from your app on the auth0 dashboard
+  jwksUri: process.env.JWKS_URI,
+});
+
+// Match the JWT's key to your Auth0 Account Key so we can validate it
+// function getKey(header, callback) {
+//   console.log('header', header.kid, callback);
+//   client.getSigningKey(header.kid, function (err, key) {
+//     console.log('KKKKKKKKKKk',key);
+//     const signingKey = key.publicKey || key.rsaPublicKey;
+//     callback(null, signingKey);
+//   });
+// }
+function getKey(header, callback){
+  client.getSigningKey(header.kid, function(err, key) {
+    var signingKey = key.publicKey || key.rsaPublicKey;
+    callback(null, signingKey);
+  });
+}
+
 
 // This is a special function for express called "Middleware"
 // We can simply "use()" this in our server
@@ -18,26 +48,6 @@ function verifyUser(request, response, next) {
   } catch (error) {
     next('Not Authorized');
   }
-}
-
-
-// =============== HELPER METHODS, pulled from the jsonwebtoken documentation =================== //
-//                 https://www.npmjs.com/package/jsonwebtoken                                     //
-
-// Define a client, this is a connection to YOUR auth0 account, using the URL given in your dashboard
-const client = jwksClient({
-  // this url comes from your app on the auth0 dashboard
-  jwksUri: process.env.JWKS_URI,
-});
-
-// Match the JWT's key to your Auth0 Account Key so we can validate it
-function getKey(header, callback) {
-  console.log('header', header.kid, callback);
-  client.getSigningKey(header.kid, function (err, key) {
-    console.log('KKKKKKKKKKk',key);
-    const signingKey = key.publicKey || key.rsaPublicKey;
-    callback(null, signingKey);
-  });
 }
 
 
